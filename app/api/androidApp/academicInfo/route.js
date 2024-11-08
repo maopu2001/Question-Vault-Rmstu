8e7cdc6c05040476ac34317fa06195f931d7f3fe
maopu2001 <maopu2001@gmail.com>
2024-11-08 20:39:36 +0600

    added api for androidApp access

---
+++ app/api/androidApp/academicInfo/route.js
@@ -0,0 +1,16 @@
+import connectMongo from '@/mongoDB/connectMongo';
+import { Degree, Department, Faculty, Semester } from '@/mongoDB/indexSchema';
+import { NextResponse } from 'next/server';
+
+export async function GET(req) {
+  try {
+    await connectMongo();
+    const faculty = (await Faculty.find()) || [];
+    const department = (await Department.find().populate('faculty')) || [];
+    const degree = (await Degree.find().populate('faculty')) || [];
+    const semester = (await Semester.find().populate('degree')) || [];
+    return NextResponse.json({ faculty, department, degree, semester }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ faculty: [], department: [], degree: [], semester: [] }, { status: 500 });
+  }
+}
