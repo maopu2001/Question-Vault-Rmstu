3dcbb5a146a20d6dd2f256163105f8d3dbcde752
maopu2001 <maopu2001@gmail.com>
2024-11-22 00:27:59 +0600

    added image fetching

--- app/api/androidApp/fetchQuestion/route.js
+++ app/api/androidApp/fetchQuestion/route.js
@@ -1,12 +1,26 @@
 import connectMongo from '@/mongoDB/connectMongo';
+import { File, QuesInfo } from '@/mongoDB/indexSchema';
 import { NextResponse } from 'next/server';
 
 export async function GET(req) {
   const searchParams = new URL(req.url)?.searchParams;
   try {
     await connectMongo();
-    return NextResponse.json({ data: filteredQuesInfoList }, { status: 200 });
-  } catch (err) {
-    return NextResponse.json({ faculty: [], department: [], degree: [], semester: [] }, { status: 500 });
+    const quesId = searchParams.get('quesId') || '';
+    const quesInfo = await QuesInfo.findById(quesId);
+    if (!quesInfo) return NextResponse.json({ message: 'Question not found.' }, { status: 400 });
+    const fileList = quesInfo.fileList.sort((x, y) => x.pageNo - y.pageNo);
+
+    if (fileList.length === 0) return NextResponse.json({ data: [] }, { status: 200 });
+
+    const files = [];
+    for (let file of fileList) {
+      const fileInfo = await File.findById(file.id);
+      files.push(fileInfo);
+    }
+
+    return NextResponse.json({ data: files }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: error.message || 'Something went wrong.' }, { status: 500 });
   }
 }
