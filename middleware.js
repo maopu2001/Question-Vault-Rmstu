3834000d3eec17dc61df7dbec8e9c9d1a021431d
maopu2001 <maopu2001@gmail.com>
2025-07-14 01:10:16 +0600

    Refactor middleware.js for accessing questions easily

--- middleware.js
+++ middleware.js
@@ -1,10 +1,15 @@
-import { NextResponse } from 'next/server';
-import jwtVerify from './lib/jwtVerify';
+import { NextResponse } from "next/server";
+import jwtVerify from "./lib/jwtVerify";
 
 export async function middleware(req) {
-  const publicPaths = ['/about', '/ExamQuestionsRMSTU.apk'];
-  const adminPaths = ['/admin'];
-  const superadminPaths = ['/superadmin'];
+  const publicPaths = [
+    "/about",
+    "/ExamQuestionsRMSTU.apk",
+    "/searchQuestion",
+    "/question",
+  ];
+  const adminPaths = ["/admin"];
+  const superadminPaths = ["/superadmin"];
 
   const nextPath = req.nextUrl.pathname;
 
@@ -12,11 +17,13 @@ export async function middleware(req) {
     return NextResponse.next();
   }
 
-  if (nextPath.startsWith('/emailverification')) {
-    const id = nextPath.slice('/emailverification'.length + 1);
+  if (nextPath.startsWith("/emailverification")) {
+    const id = nextPath.slice("/emailverification".length + 1);
     try {
-      const res = await fetch(`${req.nextUrl.origin}/api/auth/checkTempAuth?id=${id}`);
-      if (!res.ok) return NextResponse.redirect(new URL('/not_found', req.url));
+      const res = await fetch(
+        `${req.nextUrl.origin}/api/auth/checkTempAuth?id=${id}`
+      );
+      if (!res.ok) return NextResponse.redirect(new URL("/not_found", req.url));
       return NextResponse.next();
     } catch (error) {
       return NextResponse.next();
@@ -24,26 +31,29 @@ export async function middleware(req) {
     // return NextResponse.next();
   }
 
-  const token = req.cookies.get('token')?.value;
+  const token = req.cookies.get("token")?.value;
 
-  if (nextPath === '/login' || nextPath === '/signup') {
-    if (token) return NextResponse.redirect(new URL('/dashboard', req.url));
+  if (nextPath === "/login" || nextPath === "/signup") {
+    if (token) return NextResponse.redirect(new URL("/dashboard", req.url));
     else return NextResponse.next();
   }
 
-  if (nextPath === '/changepassword') {
+  if (nextPath === "/changepassword") {
     try {
-      const passChangeToken = req.cookies.get('passChangeToken')?.value;
+      const passChangeToken = req.cookies.get("passChangeToken")?.value;
       await jwtVerify(passChangeToken, process.env.JWT_SECRET);
       return NextResponse.next();
     } catch (error) {
-      return NextResponse.redirect(new URL('/login', req.url));
+      return NextResponse.redirect(new URL("/login", req.url));
     }
   }
 
+  if (nextPath === "/")
+    return NextResponse.redirect(new URL("/searchQuestion", req.url));
+
   if (!token) {
-    console.error('Token not found');
-    return NextResponse.redirect(new URL('/login', req.url));
+    console.error("Token not found");
+    return NextResponse.redirect(new URL("/login", req.url));
   }
 
   try {
@@ -51,36 +61,42 @@ export async function middleware(req) {
     const payload = await jwtVerify(token, process.env.JWT_SECRET);
 
     if (payload) {
-      if (nextPath === '/') return NextResponse.redirect(new URL('/dashboard', req.url));
+      if (nextPath === "/")
+        return NextResponse.redirect(new URL("/dashboard", req.url));
     }
-    if (payload.role === 'user' && nextPath === '/dashboard')
-      return NextResponse.redirect(new URL('/searchQuestion', req.url));
+    if (payload.role === "user" && nextPath === "/dashboard")
+      return NextResponse.redirect(new URL("/searchQuestion", req.url));
 
-    if (payload.role === 'superadmin') {
-      if (nextPath === '/dashboard') return NextResponse.redirect(new URL('/superadmin/dashboard', req.url));
-      return NextResponse.next();
-    }
-
-    if (payload.role === 'admin' && !superadminPaths.some((path) => nextPath.startsWith(path))) {
-      if (nextPath === '/dashboard') return NextResponse.redirect(new URL('/admin/dashboard', req.url));
+    if (payload.role === "superadmin") {
+      if (nextPath === "/dashboard")
+        return NextResponse.redirect(new URL("/superadmin/dashboard", req.url));
       return NextResponse.next();
     }
 
     if (
-      payload.role === 'user' &&
+      payload.role === "admin" &&
+      !superadminPaths.some((path) => nextPath.startsWith(path))
+    ) {
+      if (nextPath === "/dashboard")
+        return NextResponse.redirect(new URL("/admin/dashboard", req.url));
+      return NextResponse.next();
+    }
+
+    if (
+      payload.role === "user" &&
       !superadminPaths.some((path) => nextPath.startsWith(path)) &&
       !adminPaths.some((path) => nextPath.startsWith(path))
     ) {
       return NextResponse.next();
     }
 
-    return NextResponse.redirect(new URL('/unauthorized', req.url));
+    return NextResponse.redirect(new URL("/unauthorized", req.url));
   } catch (error) {
-    console.error('Token verification failed');
-    return NextResponse.redirect(new URL('/login', req.url));
+    console.error("Token verification failed");
+    return NextResponse.redirect(new URL("/login", req.url));
   }
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
